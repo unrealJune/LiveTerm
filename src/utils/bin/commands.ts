82581459a8fca7b346fc648ad0df771dd893897e
ToshBoss <mailtojoshuaphilip@gmail.com>
2022-05-24 16:41:36 -0400

    done done

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -38,19 +38,39 @@ More about me:
 'readme' - my github readme.`;
 };
 
+
+export const projects = async (args: string[]): Promise<string> => {
+  return ` 
+  Capital Educators - SAT/ACT Teacher
+  \tProfessionally taught small, skill level separated
+  \tgroups of high schoolers comprehensive strategies 
+  \tand required knowledge for use on the SAT and ACT.
+
+  \tResponsible for communication to parents,
+  \tstudents, and inter-departmental organizations.
+
+  \t2021-Ongoing | Bethesda, MD 
+
+  Evangelion
+  \tDeveloped turn-key cross platform application (iOS, Android, Web)
+  \tdesigned to assist local communities in planning rideshares,
+  \torganize cultural events, host materials, and communicate.
+  \tProgrammed using Dart and the Flutter framework and utilized 
+  \tFirestore backend.
+
+  \t2018-Ongoing | Mount Olive, NJ
+
+
+  There's a lot more to be found on my resume <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.projects_url}" target="_blank">here</a></u>. Take a look!
+
+  `;
+};
+
 export const resume = async (args: string[]): Promise<string> => {
   window.open(`${config.resume_url}`);
   return 'Opening resume...';
 };
 
-// Donate
-export const donate = async (args: string[]): Promise<string> => {
-  return `thank you for your interest. 
-here are the ways you can support my work:
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
-`;
-};
 
 // Contact
 export const email = async (args: string[]): Promise<string> => {
@@ -64,33 +84,6 @@ export const github = async (args: string[]): Promise<string> => {
   return 'Opening github...';
 };
 
-export const linkedin = async (args: string[]): Promise<string> => {
-  window.open(`https://www.linkedin.com/in/${config.social.linkedin}/`);
-
-  return 'Opening linkedin...';
-};
-
-// Search
-export const google = async (args: string[]): Promise<string> => {
-  window.open(`https://google.com/search?q=${args.join(' ')}`);
-  return `Searching google for ${args.join(' ')}...`;
-};
-
-export const duckduckgo = async (args: string[]): Promise<string> => {
-  window.open(`https://duckduckgo.com/?q=${args.join(' ')}`);
-  return `Searching duckduckgo for ${args.join(' ')}...`;
-};
-
-export const bing = async (args: string[]): Promise<string> => {
-  window.open(`https://bing.com/search?q=${args.join(' ')}`);
-  return `Wow, really? You are using bing for ${args.join(' ')}?`;
-};
-
-export const reddit = async (args: string[]): Promise<string> => {
-  window.open(`https://www.reddit.com/search/?q=${args.join(' ')}`);
-  return `Searching reddit for ${args.join(' ')}...`;
-};
-
 // Typical linux commands
 export const echo = async (args: string[]): Promise<string> => {
   return args.join(' ');
@@ -101,16 +94,14 @@ export const whoami = async (args: string[]): Promise<string> => {
 };
 
 export const ls = async (args: string[]): Promise<string> => {
-  return `a
-bunch
-of
-fake
-directories`;
+  return `
+  /support/
+  /trans/
+  /programmers!/`;
 };
 
 export const cd = async (args: string[]): Promise<string> => {
-  return `unfortunately, i cannot afford more directories.
-if you want to help, you can type 'donate'.`;
+  return `Segmentation Fault (Core Dumped). \nJust kidding! There aren't any folders that you'd want to access anyway.`;
 };
 
 export const date = async (args: string[]): Promise<string> => {
@@ -149,11 +140,14 @@ __ \\ \\  \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\_|/__       \\ \\   ____\\ \\ 
 \\ \\________\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\       \\ \\__\\    \\ \\__\\ \\__\\ \\__\\ \\_______\\ \\__\\ \\__\\   
 \\|________|\\|_______|\\|__| \\|__|\\|_______|        \\|__|     \\|__|\\|__|\\|__|\\|_______|\\|__|\\|__|   
                                                                                                   
-                                                                                                                                                                             
-                                                                                            
+                                                                                
+Hey! My name's June, a student at the University of Maryland, College Park.
+I'm studying computer science, and graduating in 2024. Here's my portfolio!
+
+Tips: Try using the "projects" command!
 
 Type 'help' to see the list of available commands.
-Type 'sumfetch' to display summary.
+Type 'sumfetch' to display a summary of everything here (this one is cool!).
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
 `;
 };
